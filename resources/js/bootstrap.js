9ef7d957cf137661c09dcebae70504d1257dd789
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2025-01-22 15:33:16 +0900

    認証の修正

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,3 +5,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Accept'] = 'application/json';
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
